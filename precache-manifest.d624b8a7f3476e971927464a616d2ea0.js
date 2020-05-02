387379b6d82d83c8efc1be328da59e386cd151ba
Prashant27011998 <prashantjha9610533986@gmail.com>
2020-05-02 15:49:03 +0530

    Updates

---
+++ precache-manifest.d624b8a7f3476e971927464a616d2ea0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0222835466176994c0dd03190fc71a24",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "abdcbba08c2f65f83fbb",
+    "url": "/my-app/static/css/main.3c313a39.chunk.css"
+  },
+  {
+    "revision": "204e398031b5c1b01343",
+    "url": "/my-app/static/js/2.faa87697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.faa87697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abdcbba08c2f65f83fbb",
+    "url": "/my-app/static/js/main.ede3ac33.chunk.js"
+  },
+  {
+    "revision": "6b90711d51ae396ac389",
+    "url": "/my-app/static/js/runtime-main.8c451e69.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
